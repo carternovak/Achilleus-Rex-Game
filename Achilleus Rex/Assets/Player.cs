ff1184f9716d4e28769ff089ad22e12681e33015
novakc <novakc@iu.edu>
2022-04-10 19:44:22 -0400

    commit

--- Achilleus Rex/Assets/Player.cs
+++ Achilleus Rex/Assets/Player.cs
@@ -1,30 +1,131 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public CharacterController controller;
-    public Transform cam;
-    public float speed = 6f;
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
-    // Update is called once per frame
+    [SerializeField] private float moveSpeed = 1;
+    [SerializeField] private float lookSensitivity = 5;
+    [SerializeField] private float jumpHeight = 10;
+    [SerializeField] private float gravity = 9.81f;
+
+    private Vector2 moveVector;
+    private Vector2 lookVector;
+    private Vector3 rotation;
+    private float verticalVelocity;
+    private CharacterController characterController;
+    private Animator animator;
+
+    private Vector3 moveDir = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+    
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Move();
+        //Run();
+        Rotate();
+    }
 
-        if(direction.magnitude >= 0.1f)
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        //moveVector = context.ReadValue<Vector2>();
+        if(context.ReadValueAsButton())
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+            //moveVector = context.ReadValue<Vector2>();
         }
     }
 
+    /*private void Run()
+    {
+        verticalVelocity += -gravity*Time.deltaTime;
+
+        if(characterController.isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -0.1f*gravity*Time.deltaTime;
+        }
+
+        Vector3 move = transform.right*moveVector.x + transform.forward*moveVector.y + transform.up*verticalVelocity;
+        characterController.Move(move*moveSpeed*Time.deltaTime);
+    }*/
+
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveVector = context.ReadValue<Vector2>();
+        if(moveVector.magnitude > 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        moveDir = new Vector3(moveVector.x, 0, moveVector.y);
+        if (moveDir.magnitude > 1.0f)
+        {
+            moveDir = moveDir.normalized;
+        }
+        moveDir = transform.InverseTransformDirection(moveDir);
+        Debug.Log(moveDir);
+        transform.LookAt(moveDir);
+
+    }
+
+    private void Move()
+    {
+        verticalVelocity += -gravity*Time.deltaTime;
+
+        if(characterController.isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -0.1f*gravity*Time.deltaTime;
+        }
+
+        Vector3 move = transform.right*moveVector.x + transform.forward*moveVector.y + transform.up*verticalVelocity;
+        characterController.Move(move*moveSpeed*Time.deltaTime);
+        
+        //transform.rotation = Quaternion.LookRotation(move);
+
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookVector = context.ReadValue<Vector2>();
+/*         Vector3 currPosition = transform.position;
+        Vector3 newPos = new Vector3(moveVector.x, 0, moveVector.y);
+        Vector3 posToLookAt = currPosition + newPos; */
+
+        //transform.LookAt(posToLookAt);
+    }
+
+    public void Rotate()
+    {
+        rotation.y += lookVector.x*lookSensitivity*Time.deltaTime;
+        transform.localEulerAngles = rotation;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(characterController.isGrounded && context.performed)
+        {
+            animator.Play("Jump");
+            //Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        verticalVelocity = Mathf.Sqrt(jumpHeight*gravity);
+    }
 }
---
+++ AchilleusRex2/Assets/PlayerInput.cs
@@ -0,0 +1,285 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInput"",
+    ""maps"": [
+        {
+            ""name"": ""CharacterControls"",
+            ""id"": ""9791c7a1-b471-4ef3-8953-97ff224ac306"",
+            ""actions"": [
+                {
+                    ""name"": ""Walk"",
+                    ""type"": ""Value"",
+                    ""id"": ""8ae6e8c0-d6a4-4ea0-8a23-360f265ecb73"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Run"",
+                    ""type"": ""Value"",
+                    ""id"": ""d1520f09-d234-41c4-b07a-fdcf2d8423f3"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Run 2"",
+                    ""type"": ""Button"",
+                    ""id"": ""c510e3c2-1ab3-4e58-9115-03557f9f87da"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""32ff96c6-3f7b-4d67-b13c-3055fb1f2f9c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""51146051-227f-4278-bc51-2e717b2d56ab"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": ""StickDeadzone(max=0.499)"",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""4dd7bcd4-6dad-4e5d-8894-36a3e9bbbb3b"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""ad2a927d-2edc-4fbc-9829-dea5c796bc9c"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""1d33bc23-67e1-4616-a959-aa89db247d1f"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""c20eeeb6-7ea4-4bc7-a865-1393bc6b2d69"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""8b2a9d87-16a8-44f7-b334-6ee521f7287f"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Walk"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""2a0c5ace-b177-4c0f-afb5-b2f88b01024e"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": ""StickDeadzone(min=0.5,max=0.1)"",
+                    ""groups"": """",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""fea68438-50cc-49e8-b384-c3232dd75951"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""6d5fe253-d698-4829-a309-494ad61fc25a"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1b30499b-ae67-4d2a-a348-54ce83d371dd"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": ""Press(behavior=2)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Run 2"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // CharacterControls
+        m_CharacterControls = asset.FindActionMap("CharacterControls", throwIfNotFound: true);
+        m_CharacterControls_Walk = m_CharacterControls.FindAction("Walk", throwIfNotFound: true);
+        m_CharacterControls_Run = m_CharacterControls.FindAction("Run", throwIfNotFound: true);
+        m_CharacterControls_Run2 = m_CharacterControls.FindAction("Run 2", throwIfNotFound: true);
+        m_CharacterControls_Jump = m_CharacterControls.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // CharacterControls
+    private readonly InputActionMap m_CharacterControls;
+    private ICharacterControlsActions m_CharacterControlsActionsCallbackInterface;
+    private readonly InputAction m_CharacterControls_Walk;
+    private readonly InputAction m_CharacterControls_Run;
+    private readonly InputAction m_CharacterControls_Run2;
+    private readonly InputAction m_CharacterControls_Jump;
+    public struct CharacterControlsActions
+    {
+        private @PlayerInput m_Wrapper;
+        public CharacterControlsActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Walk => m_Wrapper.m_CharacterControls_Walk;
+        public InputAction @Run => m_Wrapper.m_CharacterControls_Run;
+        public InputAction @Run2 => m_Wrapper.m_CharacterControls_Run2;
+        public InputAction @Jump => m_Wrapper.m_CharacterControls_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_CharacterControls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(CharacterControlsActions set) { return set.Get(); }
+        public void SetCallbacks(ICharacterControlsActions instance)
+        {
+            if (m_Wrapper.m_CharacterControlsActionsCallbackInterface != null)
+            {
+                @Walk.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnWalk;
+                @Walk.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnWalk;
+                @Walk.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnWalk;
+                @Run.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun;
+                @Run.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun;
+                @Run.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun;
+                @Run2.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun2;
+                @Run2.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun2;
+                @Run2.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnRun2;
+                @Jump.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_CharacterControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Walk.started += instance.OnWalk;
+                @Walk.performed += instance.OnWalk;
+                @Walk.canceled += instance.OnWalk;
+                @Run.started += instance.OnRun;
+                @Run.performed += instance.OnRun;
+                @Run.canceled += instance.OnRun;
+                @Run2.started += instance.OnRun2;
+                @Run2.performed += instance.OnRun2;
+                @Run2.canceled += instance.OnRun2;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public CharacterControlsActions @CharacterControls => new CharacterControlsActions(this);
+    public interface ICharacterControlsActions
+    {
+        void OnWalk(InputAction.CallbackContext context);
+        void OnRun(InputAction.CallbackContext context);
+        void OnRun2(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
