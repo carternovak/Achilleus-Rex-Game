f4161fddbc2bea1fbb48570a3551dd1c4b7a06d0
James Ashcraft <jashcra@iu.edu>
2022-04-12 18:57:15 -0400

    Merged camera functionality with player animation
    player animation still based on world, not relative to actual movement

---
+++ Achilleus Rex/Assets/CameraController.cs
@@ -0,0 +1,185 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Player player;
+
+    public Transform target;
+    public Transform camera;
+    public Transform cameraPivot;
+    private Transform myTransform;
+    private Vector3 cameraTransformPosition;
+
+    public float lookSpeed = 0.1f;
+    public float followSpeed = 0.1f;
+    public float pivotSpeed = 0.03f;
+
+    private float defaultPosition;
+    private float lookAngle;
+    private float pivotAngle;
+    public float minimumPivot = -35;
+    public float maximumPivot = 35;
+
+    public float lockedPivotPosition = 2.25f;
+    public float unlockedPivotPosition = 1.65f;
+
+    //Camera Lockon to Enemies
+    public CharacterLock currentLockOnTarget;
+    List<CharacterLock> availableTargets = new List<CharacterLock>();
+    public CharacterLock nearestLockOnTarget;
+    public CharacterLock leftLockTarget;
+    public CharacterLock rightLockTarget;
+    public float maximumLockOnDistance = 30;
+
+    public GameObject crossHair;
+    public GameObject leftTargetCrossHair;
+    public GameObject rightTargetCrossHair;
+
+    private void Awake()
+    {
+        myTransform = transform;
+        defaultPosition = camera.localPosition.z;
+        player = FindObjectOfType<Player>();
+    }
+
+    public void FollowTarget(float delta)
+    {
+        Vector3 targetPosition = Vector3.Lerp(myTransform.position, target.position, delta / followSpeed);
+        myTransform.position = targetPosition;
+    }
+
+    public void HandleCameraRotation(float delta, float mouseXInput, float mouseYInput)
+    {
+        if (player.lockedOn == false && currentLockOnTarget == null)
+        {
+            lookAngle += (mouseXInput * lookSpeed) / delta;
+            pivotAngle -= (mouseYInput * pivotSpeed) / delta;
+            pivotAngle = Mathf.Clamp(pivotAngle, minimumPivot, maximumPivot);
+
+            Vector3 rotation = Vector3.zero;
+            rotation.y = lookAngle;
+            Quaternion targetRotation = Quaternion.Euler(rotation);
+            myTransform.rotation = targetRotation;
+
+            rotation = Vector3.zero;
+            rotation.x = pivotAngle;
+
+            targetRotation = Quaternion.Euler(rotation);
+            cameraPivot.localRotation = targetRotation;
+        }
+        else
+        {
+            float velocity = 0;
+
+            Vector3 dir = currentLockOnTarget.transform.position - transform.position;
+            dir.Normalize();
+            dir.y = 0;
+
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
+            transform.rotation = targetRotation;
+
+            dir = currentLockOnTarget.transform.position - cameraPivot.position;
+            dir.Normalize();
+
+            targetRotation = Quaternion.LookRotation(dir);
+            Vector3 eulerAngle = targetRotation.eulerAngles;
+            eulerAngle.y = 0;
+            cameraPivot.localEulerAngles = eulerAngle;
+        }
+    }
+
+    public void HandleLockOn()
+    {
+        float shortestDistance = Mathf.Infinity;
+        float shortestDistanceOfLeftTarget = -Mathf.Infinity;
+        float shortestDistanceOfRightTarget = Mathf.Infinity;
+
+        Collider[] colliders = Physics.OverlapSphere(target.position, 26);
+
+        for(int i=0; i< colliders.Length; i++)
+        {
+            CharacterLock character = colliders[i].GetComponent<CharacterLock>();
+
+            if(character != null)
+            {
+                Vector3 lockTargetDirection = character.transform.position - target.position;
+                float distanceFromTarget = Vector3.Distance(target.position, character.transform.position);
+                float viewableAngle = Vector3.Angle(lockTargetDirection, camera.forward);
+
+                if (character.transform.root != target.transform.root && viewableAngle > -90 && viewableAngle < 90 && distanceFromTarget <= maximumLockOnDistance)
+                {
+                    availableTargets.Add(character);
+                    Debug.Log("Available Target Found!");
+                }
+            } 
+        }
+
+        for (int j=0; j<availableTargets.Count; j++)
+        {
+            float distanceFromTarget = Vector3.Distance(target.position, availableTargets[j].transform.position);
+
+            if(distanceFromTarget < shortestDistance)
+            {
+                shortestDistance = distanceFromTarget;
+                nearestLockOnTarget = availableTargets[j];
+            }
+
+            if (player.lockedOn)
+            {
+                //Vector3 relativeEnemyPosition = currentLockOnTarget.transform.InverseTransformPoint(availableTargets[j].transform.position);
+                //var distanceFromLeftTarget = currentLockOnTarget.transform.position.x - availableTargets[j].transform.position.x;
+                //var distanceFromRightTarget = currentLockOnTarget.transform.position.x + availableTargets[j].transform.position.x;
+
+                Vector3 relativeEnemyPosition = camera.transform.InverseTransformPoint(availableTargets[j].transform.position); //Debug.Log(relativeEnemyPosition+availableTargets[j].name);
+                var distanceFromLeftTarget = relativeEnemyPosition.x;
+                var distanceFromRightTarget = relativeEnemyPosition.x;
+
+                // if enemy is to the left
+                if (relativeEnemyPosition.x <= 0.00 && distanceFromLeftTarget > shortestDistanceOfLeftTarget && availableTargets[j] != currentLockOnTarget)
+                {
+                    shortestDistanceOfLeftTarget = distanceFromLeftTarget;
+                    leftLockTarget = availableTargets[j];
+                }
+
+                // if enemy is to the right
+                else if (relativeEnemyPosition.x >= 0.00 && distanceFromRightTarget < shortestDistanceOfRightTarget && availableTargets[j] != currentLockOnTarget)
+                {
+                    shortestDistanceOfRightTarget = distanceFromRightTarget;
+                    rightLockTarget = availableTargets[j];
+                }
+            }
+        }
+    }
+
+    public void ClearLockOnTargets()
+    {
+        availableTargets.Clear();
+        nearestLockOnTarget = null;
+        currentLockOnTarget = null;
+
+        leftLockTarget = null;
+        rightLockTarget = null;
+
+        //debug
+        leftTargetCrossHair.SetActive(false);
+        rightTargetCrossHair.SetActive(false);
+    }
+
+    public void SetCameraHeight()
+    {
+        Vector3 velocity = Vector3.zero;
+        Vector3 newLockedPosition = new Vector3(0, lockedPivotPosition);
+        Vector3 newUnlockedPosition = new Vector3(0, unlockedPivotPosition);
+
+        if(currentLockOnTarget != null)
+        {
+            cameraPivot.transform.localPosition = Vector3.SmoothDamp(cameraPivot.transform.localPosition, newLockedPosition, ref velocity, Time.deltaTime);
+        }
+        else
+        {
+            cameraPivot.transform.localPosition = Vector3.SmoothDamp(cameraPivot.transform.localPosition, newUnlockedPosition, ref velocity, Time.deltaTime);
+        }
+    }
+}
--- AchilleusRex2/Assets/Player.cs
+++ AchilleusRex2/Assets/Player.cs
@@ -1,33 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    [SerializeField] private float moveSpeed = 1;
-    [SerializeField] private float lookSensitivity = 5;
-    [SerializeField] private float jumpHeight = 10;
-    [SerializeField] private float gravity = 9.81f;
-
-    private Vector2 moveVector;
-    private Vector2 lookVector;
-    private Vector3 rotation;
-    private float verticalVelocity;
-    private CharacterController characterController;
-    private Animator animator;
-
-    private Vector3 moveDir = Vector3.zero;
-
-    void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
-    }
-    
-    void Update()
-=======
     public CharacterController controller;
 
     CameraController cameraController;
@@ -36,7 +12,7 @@ public class Player : MonoBehaviour
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-    PlayerControls inputActions;
+    PlayerInput inputActions;
 
     public bool lockOnInput;
     public bool lockedOn;
@@ -47,10 +23,10 @@ public class Player : MonoBehaviour
     {
         if(inputActions == null)
         {
-            inputActions = new PlayerControls();
-            inputActions.Gameplay.LockOn.performed += i => lockOnInput = true;
-            inputActions.Gameplay.LockOnTargetRight.performed += i => rightStickRightInput = true;
-            inputActions.Gameplay.LockOnTargetLeft.performed += i => rightStickLeftInput = true;
+            inputActions = new PlayerInput();
+            inputActions.CharacterControls.LockOn.performed += i => lockOnInput = true;
+            inputActions.CharacterControls.LockOnTargetRight.performed += i => rightStickRightInput = true;
+            inputActions.CharacterControls.LockOnTargetLeft.performed += i => rightStickLeftInput = true;
         }
 
         inputActions.Enable();
@@ -63,114 +39,22 @@ public class Player : MonoBehaviour
     }
 
     private void FixedUpdate()
->>>>>>> Stashed changes
     {
-        Move();
-        //Run();
-        Rotate();
-    }
-
-    public void OnRun(InputAction.CallbackContext context)
-    {
-        //moveVector = context.ReadValue<Vector2>();
-        if(context.ReadValueAsButton())
-        {
-            animator.SetBool("isRunning", true);
-        }
-        else
-        {
-            animator.SetBool("isRunning", false);
-            //moveVector = context.ReadValue<Vector2>();
-        }
-    }
-
-    /*private void Run()
-    {
-        verticalVelocity += -gravity*Time.deltaTime;
-
-        if(characterController.isGrounded && verticalVelocity < 0)
-        {
-            verticalVelocity = -0.1f*gravity*Time.deltaTime;
-        }
-
-<<<<<<< Updated upstream
-        Vector3 move = transform.right*moveVector.x + transform.forward*moveVector.y + transform.up*verticalVelocity;
-        characterController.Move(move*moveSpeed*Time.deltaTime);
-    }*/
-
-    
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveVector = context.ReadValue<Vector2>();
-        if(moveVector.magnitude > 0)
-        {
-            animator.SetBool("isWalking", true);
-        }
-        else
-=======
-        if (direction.magnitude >= 0.1f)
->>>>>>> Stashed changes
-        {
-            animator.SetBool("isWalking", false);
-        }
-
-        moveDir = new Vector3(moveVector.x, 0, moveVector.y);
-        if (moveDir.magnitude > 1.0f)
-        {
-            moveDir = moveDir.normalized;
-        }
-<<<<<<< Updated upstream
-        moveDir = transform.InverseTransformDirection(moveDir);
-        Debug.Log(moveDir);
-        transform.LookAt(moveDir);
-
-    }
-
-    private void Move()
-    {
-        verticalVelocity += -gravity*Time.deltaTime;
-
-        if(characterController.isGrounded && verticalVelocity < 0)
-        {
-            verticalVelocity = -0.1f*gravity*Time.deltaTime;
-        }
-
-        Vector3 move = transform.right*moveVector.x + transform.forward*moveVector.y + transform.up*verticalVelocity;
-        characterController.Move(move*moveSpeed*Time.deltaTime);
         
-        //transform.rotation = Quaternion.LookRotation(move);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-    }
-
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        lookVector = context.ReadValue<Vector2>();
-/*         Vector3 currPosition = transform.position;
-        Vector3 newPos = new Vector3(moveVector.x, 0, moveVector.y);
-        Vector3 posToLookAt = currPosition + newPos; */
-
-        //transform.LookAt(posToLookAt);
-    }
-
-    public void Rotate()
-    {
-        rotation.y += lookVector.x*lookSensitivity*Time.deltaTime;
-        transform.localEulerAngles = rotation;
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        if(characterController.isGrounded && context.performed)
+        if (direction.magnitude >= 0.1f)
         {
-            animator.Play("Jump");
-            //Jump();
-        }
-    }
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-    private void Jump()
-    {
-        verticalVelocity = Mathf.Sqrt(jumpHeight*gravity);
-=======
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        
 
         float horizontalMouse = Input.GetAxisRaw("Mouse X");
         float verticalMouse = Input.GetAxisRaw("Mouse Y");
@@ -214,6 +98,7 @@ public class Player : MonoBehaviour
         {
             cameraController.crossHair.transform.position = Camera.main.WorldToScreenPoint(cameraController.currentLockOnTarget.transform.position);
             //DEBUG
+            /*
             if(cameraController.rightLockTarget != null)
             {
                 cameraController.rightTargetCrossHair.SetActive(true);
@@ -230,6 +115,7 @@ public class Player : MonoBehaviour
             {
                 cameraController.leftTargetCrossHair.SetActive(false);
             }
+            */
             
             if (rightStickLeftInput) //If user is locked on and flicks stick to left
             {
@@ -258,6 +144,5 @@ public class Player : MonoBehaviour
         
 
         cameraController.SetCameraHeight();
->>>>>>> Stashed changes
     }
 }
